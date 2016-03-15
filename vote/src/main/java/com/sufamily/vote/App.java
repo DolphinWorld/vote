06bf41bb6087e0f8a9bf18edcdfdb67f164dc5da
Su <sujack@amazon.com>
2016-03-15 10:26:11 -0700

    Created project

---
+++ vote/src/main/java/com/sufamily/vote/App.java
@@ -0,0 +1,10 @@
+package com.sufamily.vote;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is main page for voting appliation" );
+    }
+}
